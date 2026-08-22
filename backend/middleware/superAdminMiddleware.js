f6d071262785722290842443ffc588cf2ecd795a
VsoftTraining <vsoftdevtraining@gmail.com>
2026-08-22 12:11:00 +0530

    Update Super Admin Employees

--- backend/middleware/superAdminMiddleware.js
+++ backend/middleware/superAdminMiddleware.js
@@ -1,8 +1,24 @@
+// const superAdminMiddleware = (req, res, next) => {
+//   if (req.user.role !== "SUPER_ADMIN") {
+//     return res.status(403).json({
+//       success: false,
+//       message: "Only Super Admin can perform this action.",
+//     });
+//   }
+
+//   next();
+// };
+
+// module.exports = superAdminMiddleware;
+
 const superAdminMiddleware = (req, res, next) => {
-  if (req.user.role !== "SUPER_ADMIN") {
+  if (
+    req.user.role !== "SUPER_ADMIN" &&
+    req.user.role !== "EMPLOYEE"
+  ) {
     return res.status(403).json({
       success: false,
-      message: "Only Super Admin can perform this action.",
+      message: "Access denied.",
     });
   }
 
